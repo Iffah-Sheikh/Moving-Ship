1a7dbf682265114f06e116972335decbfc52c880
Iffah-Sheikh <87290778+Iffah-Sheikh@users.noreply.github.com>
2021-07-22 16:00:30 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+var seaImg;
+var shipImg, ship;
+var sea;
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg = loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  sea = createSprite(0,0,400,400)
+  sea.addImage(seaImg);
+
+  sea.velocityX = 4;
+  if(sea.x < 0) {
+      sea.x = sea.width/2;
+   }
+
+  ship = createSprite(150,160,200,200);
+  ship.scale = 0.25;
+  ship.addAnimation("running", shipImg);
+}
+
+function draw() {
+  background("blue");
+  drawSprites();
+
+  
+}
